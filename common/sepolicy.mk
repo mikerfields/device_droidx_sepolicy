ad1a03c57f532d65571ebb7e1395d49486207ab1
slogar25 <michaelslogar@gmail.com>
2025-03-25 09:32:52 +0000

    adapt for komodo

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -18,11 +18,13 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/droidx/sepolicy/common/dynamic \
-    device/droidx/sepolicy/common/system
+    device/droidx/sepolicy/common/system \
+	hardware/google/pixel-sepolicy/turbo_adapter
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/droidx/sepolicy/common/dynamic \
-    device/droidx/sepolicy/common/vendor
+    device/droidx/sepolicy/common/vendor \
+	hardware/google/pixel-sepolicy/turbo_adapter
 endif
 
 # Include atv rules on atv product
